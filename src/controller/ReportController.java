99379d0e8f57204caf38094ec42529a7c5776552
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-10-26 13:40:38 +0200

    Added annotation to ReportController

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -1,12 +1,15 @@
 package controller;
 
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.sql.Connection;
 
 /**
  * Created by Niki on 2016-10-26.
  *
  * @author Niki
  */
+@WebServlet(name = "ReportController", urlPatterns = {"/ReportController"})
 public class ReportController extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
