ab5a4714631a4a578479e3119c5d53e2fdaec494
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-10-26 10:14:29 +0200

    added DB connection

---
+++ src/data/DB.java
@@ -0,0 +1,38 @@
+package data;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by Niki on 2016-10-26.
+ *
+ * @author Niki
+ */
+public class DB {
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String ip = "localhost";
+    private static String db = "sundebygninger";
+    private static int port = 3306;
+    private static String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
+    private static String user = "test";
+    private static String password = "password";
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                MysqlDataSource dataSource = new MysqlDataSource();
+                dataSource.setUser(user);
+                dataSource.setPassword(password);
+                dataSource.setServerName(ip);
+                dataSource.setDatabaseName(db);
+                conn = dataSource.getConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+}
