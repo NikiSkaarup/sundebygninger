67d5644719ffa80d10ff7a6421b1b315ec2a1ff4
JamieEgeberg <jamie_egeberg@hotmail.com>
2016-10-27 10:55:58 +0200

    init script updated with large initial letters

--- database/InitScript.sql
+++ database/InitScript.sql
@@ -1,19 +1,19 @@
 insert into Org(Name, Phone)
 values('Hvidovre Kommune', '46282828'),('Hansen A/S', '66666666');
 
-insert into building(FkOrgId,Name,Address, ConstructionYear, CurrentUse, Area, PreviousUse)
+insert into Building(FkOrgId,Name,Address, ConstructionYear, CurrentUse, Area, PreviousUse)
 values(1,'Hvidovre Bibliotek','Hovedgaden 5', '1995-01-01', 'Bibliotek', 200, 'Lager'),
 (2,'Hansen Seaside Apts','Havnevej 45-55', '2013-10-10', 'Lejligheder', 3500, '');
 
-insert into role(Name)
+insert into Role(Name)
 values('Customer'), ('Employee'),('Admin');
 
-insert into user(Name, Password, Phone, Email, FkRoleId,FkOrgId)
+insert into User(Name, Password, Phone, Email, FkRoleId,FkOrgId)
 values('Henning Flemmingsen','Henny42!','37573717','hefle@hvikom.dk',1,1),
 ('Fritz Frandsen','Fr4pst4r!','62636564','frfr@hansenapts.dk',1,2);
 
-insert into damagetype(Name)
+insert into DamageType(Name)
 values('Fugt'),('Råd og svamp'),('Skimmel'),('Brand'),('Andet');
 
-insert into servicetype(Name)
+insert into ServiceType(Name)
 values('Sundhedstjek'),('Skadetjek'),('Reperation'),('Grafitti fjernelse'),('Andet');
