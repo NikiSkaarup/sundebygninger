c2269d2a4bc987b22307464d05ba5e1e8c61d24a
JamieEgeberg <jamie_egeberg@hotmail.com>
2016-10-28 13:50:46 +0200

    Report  controll update

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -7,7 +7,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,7 +31,7 @@ public class ReportController extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         try {
             Report report = new Report();
-            
+
             String b = request.getParameter("building");
             PreparedStatement pstmt = DB.getConnection().prepareStatement("select Id from building where Name=?");
             pstmt.setString(1, b);
@@ -37,7 +39,7 @@ public class ReportController extends javax.servlet.http.HttpServlet {
             rs.next();
             Building building = new Building();
             building.setId(rs.getInt(1));
-            
+
             HashMap<Integer, Comment> comments = new HashMap<>();
             Comment roof = new Comment();
             roof.setComment(request.getParameter("roof"));
@@ -45,33 +47,35 @@ public class ReportController extends javax.servlet.http.HttpServlet {
             Comment outerWalls = new Comment();
             roof.setComment(request.getParameter("outerWalls"));
             comments.put(Integer.SIZE, outerWalls);
-                        
+
             HashMap<Integer, Room> rooms = new HashMap<>();
             Room r = new Room();
             r.setNum(request.getParameter("room"));
             r.setDamage(request.getParameter("optradio").equals("Ja"));
-            r.setWhen((TimeStamp) request.getParameter("date"));
+            Date d = new Date(request.getParameter("date"));
+            r.setWhen(new java.sql.Timestamp(d.getTime()));
             r.setWhere(request.getParameter("location"));
             r.setWhathappend(request.getParameter("incident"));
             r.setWhatwasfixed(request.getParameter("repair"));
-            //TODO: damagetypes from checkboxes
+            // TODO: damagetypes from checkboxes
+            r.setMoistureScan(request.getParameter("moistureScan"));
+            r.setMoistureTarget(request.getParameter("measureSpot"));
             r.setReport(report);
-            
+
             Calendar calendar = Calendar.getInstance();
             java.util.Date now = calendar.getTime();
             java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
             Timestamp submission = currentTimestamp;
-            
-            
+
             report.setBuilding(building);
             report.setComments(comments);
             report.setRooms(rooms);
             report.setSubmission(submission);
             reports.put(Integer.SIZE, report);
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(ReportController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
