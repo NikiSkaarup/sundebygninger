61b492e372dfa5e760bcc5ac98b62d6923960dba
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-11-02 13:12:21 +0100

    getbuilding(s) now works

--- src/db/Dal.java
+++ src/db/Dal.java
@@ -5,6 +5,10 @@ import model.Org;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,14 +28,49 @@ public class Dal {
     }
 
     public Building getBuilding(int id) {
+        String sql = "SELECT Id, Name, Address, ConstructionYear, CurrentUse, Area, PreviousUse FROM Building WHERE Id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return constructBuilding(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public List<Building> getBuildings(Org org) {
+        String sql = "SELECT Id, Name, Address, ConstructionYear, CurrentUse, Area, PreviousUse FROM Building WHERE FkOrgId=?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            List<Building> buildings = new ArrayList<>();
+            stmt.setInt(1, org.getId());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                buildings.add(constructBuilding(rs));
+            }
+            return buildings;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public List<Building> getBuildings(Org org, int count) {
+        String sql = "SELECT Id, Name, Address, ConstructionYear, CurrentUse, Area, PreviousUse FROM Building WHERE FkOrgId=? LIMIT ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            List<Building> buildings = new ArrayList<>();
+            stmt.setInt(1, org.getId());
+            stmt.setInt(2, count);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                buildings.add(constructBuilding(rs));
+            }
+            return buildings;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -41,6 +80,27 @@ public class Dal {
 
     public boolean updateBuilding(Building b) {
         return false;
+    }
+
+    private Building constructBuilding(ResultSet rs) {
+        try {
+            Building building = new Building();
+            building.setId(rs.getInt("Id"));
+            building.setName(rs.getString("Name"));
+            building.setAddress(rs.getString("Address"));
+            building.setConstructionYear(rs.getTimestamp("ConstructionYear"));
+            building.setCurrentUse(rs.getString("CurrentUse"));
+            building.setArea(rs.getString("Area"));
+            building.setPreviousUse(rs.getString("PreviousUse"));
+
+            Org org = new Org();
+            org.setId(rs.getInt("FkOrgId"));
+            building.setOrg(org);
+            return building;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public User getUser(int id) {
