a16c50423a385a672a0e45d5b6141529235419db
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-11-03 12:02:41 +0100

    Updated version of DAL

--- src/db/DAL.java
+++ src/db/DAL.java
@@ -2,10 +2,7 @@ package db;
 
 import model.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,9 +26,9 @@ public class DAL {
     }
 
     public Building getBuilding(int id) {
-        String sql = "SELECT Id, `Name`, Address, ConstructionYear, " +
+        String query = "SELECT Id, `Name`, Address, ConstructionYear, " +
                 "CurrentUse, Area, PreviousUse FROM `Building` WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next())
@@ -47,14 +44,14 @@ public class DAL {
     }
 
     public List<Building> getBuildings(Org org, int count) {
-        String sql = "SELECT Id, `Name`, Address, ConstructionYear, " +
+        String query = "SELECT Id, `Name`, Address, ConstructionYear, " +
                 "CurrentUse, Area, PreviousUse FROM `Building`";
         if (org != null)
-            sql += " WHERE FkOrgId=?";
-        sql += " ORDER BY Submission DESC";
+            query += " WHERE FkOrgId=?";
+        query += " ORDER BY Submission DESC";
         if (org != null && count > 0)
-                sql += " LIMIT ?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            query += " LIMIT ?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             List<Building> list = new ArrayList<>();
             if (org != null) {
                 stmt.setInt(1, org.getId());
@@ -73,10 +70,11 @@ public class DAL {
 
     public int insertBuilding(Building b) {
         int id = -1;
-        String sql = "INSERT INTO `Building` (`Name`, Address, " +
+        String query = "INSERT INTO `Building` (`Name`, Address, " +
                 "ConstructionYear, CurrentUse, Area, PreviousUse, FkOrgId) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?); SELECT LAST_INSERT_ID() AS Id;";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query, Statement
+                .RETURN_GENERATED_KEYS)) {
             stmt.setString(1, b.getName());
             stmt.setString(2, b.getAddress());
             stmt.setTimestamp(3, b.getConstructionYear());
@@ -84,7 +82,9 @@ public class DAL {
             stmt.setString(5, b.getArea());
             stmt.setString(6, b.getPreviousUse());
             stmt.setInt(7, b.getOrg().getId());
-            ResultSet rs = stmt.executeQuery();
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
             if (rs.next())
                 id = rs.getInt("Id");
         } catch (SQLException e) {
@@ -94,10 +94,10 @@ public class DAL {
     }
 
     public boolean updateBuilding(Building b) {
-        String sql = "UPDATE `Building` SET `Name`=?, Address=?, " +
+        String query = "UPDATE `Building` SET `Name`=?, Address=?, " +
                 "ConstructionYear=?, CurrentUse=?, Area=?, PreviousUse=?, " +
                 "FkOrgId=? WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, b.getName());
             stmt.setString(2, b.getAddress());
             stmt.setTimestamp(3, b.getConstructionYear());
@@ -136,9 +136,10 @@ public class DAL {
     }
 
     public User getUser(int id) {
-        String sql = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId FROM" +
+        String query = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId " +
+                "FROM" +
                 " `User` WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next())
@@ -150,10 +151,11 @@ public class DAL {
     }
 
     public User getUserLogin(String email, String pass) {
-        String sql = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId FROM" +
+        String query = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId " +
+                "FROM" +
                 " `User` WHERE Email=? AND Password=? GROUP BY FkOrgId ORDER " +
                 "BY `Name`";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, email);
             stmt.setString(2, pass);
             ResultSet rs = stmt.executeQuery();
@@ -174,14 +176,15 @@ public class DAL {
     }
 
     public List<User> getUsers(Org org, int count) {
-        String sql = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId FROM" +
+        String query = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId " +
+                "FROM" +
                 " `User`";
         if (org != null) {
-            sql += " WHERE FkOrgId=?";
+            query += " WHERE FkOrgId=?";
             if (count > 0)
-                sql += " LIMIT ?";
+                query += " LIMIT ?";
         }
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             if (org != null) {
                 stmt.setInt(1, org.getId());
                 if (count > 0)
@@ -200,10 +203,10 @@ public class DAL {
 
     public int insertUser(User u) {
         int id = -1;
-        String sql = "INSERT INTO `User` (`Name`, Email, Phone, FkRoleId, " +
+        String query = "INSERT INTO `User` (`Name`, Email, Phone, FkRoleId, " +
                 "FkOrgId) VALUES (?, ?, ?, ?, ?); SELECT LAST_INSERT_ID() AS " +
                 "Id;";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, u.getName());
             stmt.setString(2, u.getEmail());
             stmt.setString(3, u.getPhone());
@@ -219,9 +222,9 @@ public class DAL {
     }
 
     public boolean updateUser(User u) {
-        String sql = "UPDATE `User` SET `Name`=?, Email=?, Phone=?, " +
+        String query = "UPDATE `User` SET `Name`=?, Email=?, Phone=?, " +
                 "FkRoleId=?, FkOrgId=? WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, u.getName());
             stmt.setString(2, u.getEmail());
             stmt.setString(3, u.getPhone());
@@ -259,9 +262,9 @@ public class DAL {
     }
 
     public Image getImage(int id) {
-        String sql = "SELECT Id, `Name`, Path, FkBuildingId FROM `Image` " +
+        String query = "SELECT Id, `Name`, Path, FkBuildingId FROM `Image` " +
                 "WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next())
@@ -281,13 +284,13 @@ public class DAL {
     }
 
     public List<Image> getImages(Building b, int count) {
-        String sql = "SELECT Id, `Name`, Path, FkBuildingId FROM `Image`";
+        String query = "SELECT Id, `Name`, Path, FkBuildingId FROM `Image`";
         if (b != null) {
-            sql += " WHERE FkBuildingId=?";
+            query += " WHERE FkBuildingId=?";
             if (count > 0)
-                sql += " LIMIT ?";
+                query += " LIMIT ?";
         }
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             if (b != null) {
                 stmt.setInt(1, b.getId());
                 if (count > 0)
@@ -306,15 +309,16 @@ public class DAL {
 
     public int insertImage(Image i) {
         int id = -1;
-        String sql = "INSERT INTO `Image` (`Name`, Path, FkBuildingId) VALUES" +
+        String query = "INSERT INTO `Image` (`Name`, Path, FkBuildingId) " +
+                "VALUES" +
                 " (?, ?, ?); SELECT LAST_INSERT_ID() AS Id;";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query, Statement
+                .RETURN_GENERATED_KEYS)) {
             stmt.setString(1, i.getName());
             stmt.setString(2, i.getPath());
             stmt.setInt(3, i.getBuilding().getId());
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next())
-                id = rs.getInt("Id");
+            id = stmt.executeUpdate(query);
+            stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -322,9 +326,9 @@ public class DAL {
     }
 
     public boolean updateImage(Image i) {
-        String sql = "UPDATE `Image` SET `Name`=?, Path=?, FkBuildingId=? " +
+        String query = "UPDATE `Image` SET `Name`=?, Path=?, FkBuildingId=? " +
                 "WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, i.getName());
             stmt.setString(2, i.getPath());
             stmt.setInt(3, i.getBuilding().getId());
@@ -355,9 +359,10 @@ public class DAL {
     }
 
     public Document getDocument(int id) {
-        String sql = "SELECT Id, `Name`, Path, FkBuildingId FROM `Document` " +
+        String query = "SELECT Id, `Name`, Path, FkBuildingId FROM `Document`" +
+                " " +
                 "WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next())
@@ -377,13 +382,13 @@ public class DAL {
     }
 
     public List<Document> getDocuments(Building b, int count) {
-        String sql = "SELECT Id, `Name`, Path, FkBuildingId FROM `Document`";
+        String query = "SELECT Id, `Name`, Path, FkBuildingId FROM `Document`";
         if (b != null) {
-            sql += " WHERE FkBuildingId=?";
+            query += " WHERE FkBuildingId=?";
             if (count > 0)
-                sql += " LIMIT ?";
+                query += " LIMIT ?";
         }
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             if (b != null) {
                 stmt.setInt(1, b.getId());
                 if (count > 0)
@@ -402,9 +407,9 @@ public class DAL {
 
     public int insertDocument(Document d) {
         int id = -1;
-        String sql = "INSERT INTO `Document` (`Name`, Path, FkBuildingId) " +
+        String query = "INSERT INTO `Document` (`Name`, Path, FkBuildingId) " +
                 "VALUES (?, ?, ?); SELECT LAST_INSERT_ID() AS Id;";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, d.getName());
             stmt.setString(2, d.getPath());
             stmt.setInt(3, d.getBuilding().getId());
@@ -418,9 +423,10 @@ public class DAL {
     }
 
     public boolean updateDocument(Document d) {
-        String sql = "UPDATE `Document` SET `Name`=?, Path=?, FkBuildingId=? " +
+        String query = "UPDATE `Document` SET `Name`=?, Path=?, " +
+                "FkBuildingId=? " +
                 "WHERE Id=?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, d.getName());
             stmt.setString(2, d.getPath());
             stmt.setInt(3, d.getBuilding().getId());
@@ -442,6 +448,137 @@ public class DAL {
             Building b = new Building();
             b.setId(rs.getInt("FkBuildingId"));
             c.setBuilding(b);
+
+            return c;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Report getReport(int id) {
+        String query = "SELECT Id, FkBuildingId, FkUserId FROM `Report` " +
+                "WHERE Id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                return constructReport(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Report> getReports() {
+        return getReports(null);
+    }
+
+    public List<Report> getReports(Building b) {
+        return getReports(b, -1);
+    }
+
+    public List<Report> getReports(Building b, int count) {
+        String query = "SELECT Id, `Name`, Path, FkBuildingId FROM `Report`";
+        if (b != null) {
+            query += " WHERE FkBuildingId=?";
+            if (count > 0)
+                query += " LIMIT ?";
+        }
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            if (b != null) {
+                stmt.setInt(1, b.getId());
+                if (count > 0)
+                    stmt.setInt(2, count);
+            }
+            ResultSet rs = stmt.executeQuery();
+            List<Report> list = new ArrayList<>();
+            while (rs.next())
+                list.add(constructReport(rs));
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int insertReport(Report r) {
+        int id = -1;
+        String query = "INSERT INTO `Report` (`FkBuildingId`, `FkUserId`) " +
+                "VALUES (?, ?); SELECT LAST_INSERT_ID() AS Id;";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, r.getBuilding().getId());
+            stmt.setInt(2, r.getUser().getId());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                id = rs.getInt("Id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+
+    public boolean updateReport(Report r) {
+        String query = "UPDATE `Report` SET `FkBuildingId`=?, FkUserId=?, " +
+                "FkBuildingId=? WHERE Id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, r.getBuilding().getId());
+            stmt.setInt(2, r.getUser().getId());
+            stmt.setInt(3, r.getId());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private Report constructReport(ResultSet rs) {
+        try {
+            Report c = new Report();
+            c.setId(rs.getInt("Id"));
+            c.setSubmission(rs.getTimestamp("Submission"));
+
+            User u = new User();
+            u.setId(rs.getInt("FkUserId"));
+            c.setUser(u);
+
+            Building b = new Building();
+            b.setId(rs.getInt("FkBuildingId"));
+            c.setBuilding(b);
+
+            return c;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Room constructRoom(ResultSet rs) {
+        try {
+            Room c = new Room();
+            c.setId(rs.getInt("Id"));
+
+            return c;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Comment constructComment(ResultSet rs) {
+        try {
+            Comment c = new Comment();
+            c.setId(rs.getInt("Id"));
+            c.setPath(rs.getString("Path"));
+            c.setComment(rs.getString("Comment"));
+
+            Room room = new Room();
+            room.setId(rs.getInt("FkRoomId"));
+            c.setRoom(room);
+
+            CommentType ct = new CommentType();
+            ct.setId(rs.getInt("FkCommentTypeId"));
+            c.setCommentType(ct);
 
             return c;
         } catch (SQLException e) {
