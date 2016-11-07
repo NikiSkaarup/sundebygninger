d348d961327da7ca3e46bbea7c5cbef3d33d50fd
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-11-07 12:28:42 +0100

    Fixed bug in DAL and updated insertRequest

--- src/db/DAL.java
+++ src/db/DAL.java
@@ -155,8 +155,7 @@ public class DAL {
 
     public User getUserLogin(String email, String pass) {
         String query = "SELECT Id, `Name`, Email, Phone, FkRoleId, FkOrgId " +
-                "FROM  `User` WHERE Email=? AND Password=? GROUP BY FkOrgId " +
-                "ORDER BY `Name`";
+                "FROM  `User` WHERE Email=? AND `Password`=?";
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, email);
             stmt.setString(2, pass);
@@ -650,15 +649,20 @@ public class DAL {
 
     public int insertRequest(Request r) {
         int id = -1;
-        String query = "INSERT INTO `Request` (FkBuildingId, Submission, " +
-                "FkReportId, Description, FkServiceTypeId, FkUserId) VALUES " +
-                "(?, ?); SELECT LAST_INSERT_ID() AS Id;";
-        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+        String query = "INSERT INTO `Request` (FkBuildingId, Description, " +
+                "FkServiceTypeId) VALUES (?, ?, ?); " +
+                "SELECT LAST_INSERT_ID() AS Id;";
+        try (PreparedStatement stmt = conn.prepareStatement(query,
+                Statement.RETURN_GENERATED_KEYS)) {
             stmt.setInt(1, r.getBuilding().getId());
-            stmt.setInt(2, r.getUser().getId());
-            ResultSet rs = stmt.executeQuery();
+            stmt.setString(2, r.getDescription());
+            stmt.setInt(3, r.getServiceType().getId());
+            int changed = stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
             if (rs.next())
                 id = rs.getInt("Id");
+            rs.close();
+            stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
