0e5374092d337c19631547418b8304db5b34f560
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2016-12-09 10:51:41 +0100

    Removed new lines

--- test/util/TestHelper.java
+++ test/util/TestHelper.java
@@ -8,61 +8,61 @@ package util;
 public class TestHelper {
     public static String getCloneDBScript() {
         return "" +
-                "DROP SCHEMA IF EXISTS `junitTestDB`;\n" +
+                "DROP SCHEMA IF EXISTS `junitTestDB`;" +
                 "CREATE SCHEMA IF NOT EXISTS `junitTestDB` DEFAULT " +
-                "CHARACTER SET utf8;\n" +
-                "USE `junitTestDB`;\n" +
-                "GRANT USAGE ON `junitTestDB`.* TO 'junitTest'@'localhost';\n" +
-                "DROP USER 'junitTest'@'localhost';\n" +
+                "CHARACTER SET utf8;" +
+                "USE `junitTestDB`;" +
+                "GRANT USAGE ON `junitTestDB`.* TO 'junitTest'@'localhost';" +
+                "DROP USER 'junitTest'@'localhost';" +
                 "GRANT ALL PRIVILEGES ON junitTestDB.* TO " +
                 "'junitTest'@'localhost' " +
-                "IDENTIFIED BY 'junitTest';\n" +
-                "FLUSH PRIVILEGES;\n" +
-                "CREATE TABLE Org LIKE sundebygninger.Org;\n" +
+                "IDENTIFIED BY 'junitTest';" +
+                "FLUSH PRIVILEGES;" +
+                "CREATE TABLE Org LIKE sundebygninger.Org;" +
                 "INSERT INTO Org " +
-                "SELECT * FROM sundebygninger.Org;\n" +
-                "CREATE TABLE Role LIKE sundebygninger.Role;\n" +
+                "SELECT * FROM sundebygninger.Org;" +
+                "CREATE TABLE Role LIKE sundebygninger.Role;" +
                 "INSERT INTO Role " +
-                "SELECT * FROM sundebygninger.Role;\n" +
-                "CREATE TABLE User LIKE sundebygninger.User;\n" +
+                "SELECT * FROM sundebygninger.Role;" +
+                "CREATE TABLE User LIKE sundebygninger.User;" +
                 "INSERT INTO User " +
-                "SELECT * FROM sundebygninger.User;\n" +
-                "CREATE TABLE Building LIKE sundebygninger.Building;\n" +
+                "SELECT * FROM sundebygninger.User;" +
+                "CREATE TABLE Building LIKE sundebygninger.Building;" +
                 "INSERT INTO Building " +
-                "SELECT * FROM sundebygninger.Building;\n" +
-                "CREATE TABLE Document LIKE sundebygninger.Document;\n" +
+                "SELECT * FROM sundebygninger.Building;" +
+                "CREATE TABLE Document LIKE sundebygninger.Document;" +
                 "INSERT INTO Document " +
-                "SELECT * FROM sundebygninger.Document;\n" +
-                "CREATE TABLE Report LIKE sundebygninger.Report;\n" +
+                "SELECT * FROM sundebygninger.Document;" +
+                "CREATE TABLE Report LIKE sundebygninger.Report;" +
                 "INSERT INTO Report " +
-                "SELECT * FROM sundebygninger.Report;\n" +
-                "CREATE TABLE ServiceType LIKE sundebygninger.ServiceType;\n" +
+                "SELECT * FROM sundebygninger.Report;" +
+                "CREATE TABLE ServiceType LIKE sundebygninger.ServiceType;" +
                 "INSERT INTO ServiceType " +
-                "SELECT * FROM sundebygninger.ServiceType;\n" +
-                "CREATE TABLE Request LIKE sundebygninger.Request;\n" +
+                "SELECT * FROM sundebygninger.ServiceType;" +
+                "CREATE TABLE Request LIKE sundebygninger.Request;" +
                 "INSERT INTO Request " +
-                "SELECT * FROM sundebygninger.Request;\n" +
-                "CREATE TABLE DamageType LIKE sundebygninger.DamageType;\n" +
+                "SELECT * FROM sundebygninger.Request;" +
+                "CREATE TABLE DamageType LIKE sundebygninger.DamageType;" +
                 "INSERT INTO DamageType " +
-                "SELECT * FROM sundebygninger.DamageType;\n" +
-                "CREATE TABLE Incident LIKE sundebygninger.Incident;\n" +
+                "SELECT * FROM sundebygninger.DamageType;" +
+                "CREATE TABLE Incident LIKE sundebygninger.Incident;" +
                 "INSERT INTO Incident " +
-                "SELECT * FROM sundebygninger.Incident;\n" +
-                "CREATE TABLE Image LIKE sundebygninger.Image;\n" +
+                "SELECT * FROM sundebygninger.Incident;" +
+                "CREATE TABLE Image LIKE sundebygninger.Image;" +
                 "INSERT INTO Image " +
-                "SELECT * FROM sundebygninger.Image;\n" +
-                "CREATE TABLE Room LIKE sundebygninger.Room;\n" +
+                "SELECT * FROM sundebygninger.Image;" +
+                "CREATE TABLE Room LIKE sundebygninger.Room;" +
                 "INSERT INTO Room " +
-                "SELECT * FROM sundebygninger.Room;\n" +
+                "SELECT * FROM sundebygninger.Room;" +
                 "CREATE TABLE RoomToDamageType LIKE sundebygninger" +
-                ".RoomToDamageType;\n" +
+                ".RoomToDamageType;" +
                 "INSERT INTO RoomToDamageType " +
-                "SELECT * FROM sundebygninger.RoomToDamageType;\n" +
-                "CREATE TABLE CommentType LIKE sundebygninger.CommentType;\n" +
+                "SELECT * FROM sundebygninger.RoomToDamageType;" +
+                "CREATE TABLE CommentType LIKE sundebygninger.CommentType;" +
                 "INSERT INTO CommentType " +
-                "SELECT * FROM sundebygninger.CommentType;\n" +
-                "CREATE TABLE Comment LIKE sundebygninger.Comment;\n" +
+                "SELECT * FROM sundebygninger.CommentType;" +
+                "CREATE TABLE Comment LIKE sundebygninger.Comment;" +
                 "INSERT INTO Comment " +
-                "SELECT * FROM sundebygninger.Comment;\n";
+                "SELECT * FROM sundebygninger.Comment;";
     }
 }
